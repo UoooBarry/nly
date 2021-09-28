caaddab64c261ffeede4501a337eca65a09fbfea
uooobarry <383435763@qq.com>
2021-09-28 10:00:03 +0800

    Add recipes route

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,9 +2,15 @@ import express from "express";
 import './config/database';
 import seed from './data/seed';
 
-import categoryRouter from './routes/category'
+import categoryRouter from './routes/category';
+import recipeRouter from './routes/recipe';
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+}));
+
 const port = 3000; // default port to listen
 
 // define a route handler for the default home page
@@ -13,6 +19,7 @@ app.get("/", (req, res) => {
 });
 
 app.use('/api/categories', categoryRouter);
+app.use('/api/recipes', recipeRouter);
 
 // start the Express server
 app.listen(port, () => {

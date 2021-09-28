8ae46be48839d4cc139696b946bf9328781c896b
uooobarry <383435763@qq.com>
2021-09-28 15:18:32 +0800

    菜谱分支

--- client/src/views/Home.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="home">
-    <h1>你是我的先生</h1>
-  </div>
-</template>
-
--- server/src/config/database.ts
+++
@@ -1,8 +0,0 @@
-// getting-started.js
-import mongoose from 'mongoose';
-
-main().catch();
-
-async function main() {
-  await mongoose.connect('mongodb://localhost:27017/nly');
-}

8e8f6fb16c7329844685ee9bfea7211c4bd02c6e
uooobarry <383435763@qq.com>
2021-09-29 15:27:41 +0800

    fix api token verfication

--- server/src/middleware/apiTokenVerify.ts
+++ server/src/middleware/apiTokenVerify.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const verifyToken = (req: Request, res: Response, next: NextFunction) => {
-  if(!req.headers.API_TOKEN || req.headers.API_TOKEN !== process.env.API_TOKEN) {
+  if(!req.headers.api_token || req.headers.API_TOKEN !== process.env.API_TOKEN) {
     res.json({status: 'forbidden', message: {}}).status(403);
   } else {
     next();
